85128f7eb0f96f2992cb87223b192d444e7ced22
dushibaiyu <dushibaiyu@yahoo.com>
2018-01-12 13:21:05 +0800

    set options

--- source/kiss/array.d
+++ source/kiss/array.d
@@ -8,12 +8,6 @@ import core.stdc.string : memcpy;
 */
 size_t arrayRemove(E)(ref E[] ary, auto ref E e) {
     E tv = E.init;
-    scope void doInitVaule(ref E v){
-        static if(is(E == struct) && hasElaborateDestructor!E) {
-            destroy(v);
-        }
-        memcpy(&v,&tv,E.sizeof);
-    }
     size_t rm = 0;
     size_t site = 0;
     for (size_t j = site; j < ary.length; ++j) {
@@ -33,6 +27,28 @@ size_t arrayRemove(E)(ref E[] ary, auto ref E e) {
         fillWithMemcpy(rmed, tv);
     }
     return rm;
+}
+
+E[] removeSite(E)(ref E[] _array,size_t site) 
+    in {
+        assert(site < _array.length);
+    } body{
+        const size_t len = ary.length - 1;
+        doInitVaule(_array[site]);
+        for (size_t i = site; i < len; ++i) {
+            memcpy(&(_array[i]), &(_array[i + 1]), T.sizeof);
+        }
+        E v = E.init;
+        memcpy(&(_array[len]), &v, T.sizeof);
+        _array = _array[0..len];
+        return _array;
+    }
+
+void doInitVaule(E)(ref E v){
+    static if(is(E == struct) && hasElaborateDestructor!E) {
+        destroy(v);
+    }
+    memcpy(&v,&tv,E.sizeof);
 }
 
 //from std.experimental.allocator.package;
