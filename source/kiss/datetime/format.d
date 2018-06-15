fcadcbb17caadc58c95120f2949d9ada52c856e1
Brian Zou <zoujiaqing@gmail.com>
2018-06-15 15:31:46 +0800

    fix datetime month to short

--- source/kiss/datetime/format.d
+++ source/kiss/datetime/format.d
@@ -11,12 +11,63 @@
 
 module kiss.datetime.format;
 
+import std.datetime : Month;
+
 // return unix timestamp
 long time()
 {
     import core.stdc.time : time;
 
     return time(null);
+}
+
+short monthToShort(Month month)
+{
+    short resultMonth;
+    switch(month)
+    {
+        case Month.jan:
+            resultMonth = 1;
+            break;
+        case Month.feb:
+            resultMonth = 2;
+            break;
+        case Month.mar:
+            resultMonth = 3;
+            break;
+        case Month.apr:
+            resultMonth = 4;
+            break;
+        case Month.may:
+            resultMonth = 5;
+            break;
+        case Month.jun:
+            resultMonth = 6;
+            break;
+        case Month.jul:
+            resultMonth = 7;
+            break;
+        case Month.aug:
+            resultMonth = 8;
+            break;
+        case Month.sep:
+            resultMonth = 9;
+            break;
+        case Month.oct:
+            resultMonth = 10;
+            break;
+        case Month.nov:
+            resultMonth = 11;
+            break;
+        case Month.dec:
+            resultMonth = 12;
+            break;
+        default:
+            resultMonth = 0;
+            break;
+    }
+    
+    return resultMonth;
 }
 
 // return formated time string from timestamp
@@ -43,7 +94,8 @@ string date(string format, long timestamp = 0)
             timeString ~= (st.year.to!string)[2..$];
             break;
         case 'm':
-            timeString ~= st.month.to!string;
+            short month = monthToShort(st.month);
+            timeString ~= month < 10 ? "0" ~ month.to!string : month.to!string;
             break;
         case 'd':
             timeString ~= st.day < 10 ? "0" ~ st.day.to!string : st.day.to!string;
