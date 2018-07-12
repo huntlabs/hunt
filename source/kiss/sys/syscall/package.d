c8437c9a66d5519e660bb60a9905f7271683d354
zoujiaqing <zoujiaqing@gmail.com>
2018-07-12 21:28:15 +0800

    syscall() use extern(C)

--- source/kiss/sys/syscall/package.d
+++ source/kiss/sys/syscall/package.d
@@ -3,14 +3,19 @@ module kiss.sys.syscall;
 @system:
 version(Posix):
 
+extern (C) nothrow @nogc size_t syscall(size_t ident);
+extern (C) nothrow @nogc size_t syscall(size_t ident, size_t arg0);
+extern (C) nothrow @nogc size_t syscall(size_t ident, size_t arg0, size_t arg1);
+extern (C) nothrow @nogc size_t syscall(size_t ident, size_t arg0, size_t arg1, size_t arg2);
+extern (C) nothrow @nogc size_t syscall(size_t ident, size_t arg0, size_t arg1, size_t arg2, size_t arg3);
+extern (C) nothrow @nogc size_t syscall(size_t ident, size_t arg0, size_t arg1, size_t arg2, size_t arg3, size_t arg4);
+extern (C) nothrow @nogc size_t syscall(size_t ident, size_t arg0, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5);
+
 version(D_InlineAsm_X86_64)
 {
     version(linux) public import kiss.sys.syscall.os.linux;
     else version(OSX) public import kiss.sys.syscall.os.osx;
     else version(FreeBSD) public import kiss.sys.syscall.os.freebsd;
     else static assert(false, "Not supoorted your OS.");
-
-    public import kiss.sys.syscall.arch.x86_64;
 }
 else static assert(false, "syscall only supoorted for x86_64.");
-
