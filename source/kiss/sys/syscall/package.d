12576224cc44ead93bfe4b0ee4ea1121e454edba
zoujiaqing <zoujiaqing@gmail.com>
2018-07-12 20:01:49 +0800

    Add osx.d for syscall()
    Former-commit-id: 322200171a5f25cb9108e4e9b5d255d9501cb6f3

--- source/kiss/sys/syscall/package.d
+++ source/kiss/sys/syscall/package.d
@@ -1,20 +1,16 @@
 module kiss.sys.syscall;
 
 @system:
+version(Posix):
 
 version(D_InlineAsm_X86_64)
 {
-    version(linux) import kiss.sys.syscall.os.linux;
-    else version(OSX) import kiss.sys.syscall.os.osx;
-    else version(FreeBSD) import kiss.sys.syscall.os.freebsd;
+    version(linux) public import kiss.sys.syscall.os.linux;
+    else version(OSX) public import kiss.sys.syscall.os.osx;
+    else version(FreeBSD) public import kiss.sys.syscall.os.freebsd;
     else static assert(false, "Not supoorted your OS.");
 
-    import kiss.sys.syscall.arch.x86_64;
+    public import kiss.sys.syscall.arch.x86_64;
 }
 else static assert(false, "syscall only supoorted for x86_64.");
-
-unittest
-{
-    assert(syscall(GETPID) > 0);
-}
 
