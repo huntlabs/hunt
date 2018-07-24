e86de7514d42af22878770ac2703a51b275351e1
Heromyth <bitworld@qq.com>
2018-07-24 15:48:05 +0800

    Add more modules to hunt.io

---
+++ source/hunt/io/PipedStream.d
@@ -0,0 +1,53 @@
+module hunt.io.PipedStream;
+
+import hunt.io.common;
+import hunt.io.ByteArrayInputStream;
+import hunt.io.ByteArrayOutputStream;
+import hunt.util.common;
+import hunt.util.exception;
+
+// import kiss.logger;
+import std.algorithm;
+
+
+interface PipedStream : Closeable {
+
+	InputStream getInputStream();
+	
+	OutputStream getOutputStream();
+}
+
+class ByteArrayPipedStream : PipedStream {
+
+    private ByteArrayOutputStream outStream;
+    private ByteArrayInputStream inStream;
+    private int size;
+
+    this(int size) {
+        this.size = size;
+    }
+
+    override
+    void close() {
+        inStream = null;
+        outStream = null;
+    }
+
+    override
+    InputStream getInputStream() {
+        if (inStream is null) {
+            inStream = new ByteArrayInputStream(outStream.toByteArray());
+            outStream = null;
+        }
+        return inStream;
+    }
+
+    override
+    OutputStream getOutputStream() {
+        if (outStream is null) {
+            outStream = new ByteArrayOutputStream(size);
+        }
+        return outStream;
+    }
+
+}
---
+++ source/hunt/io/package.d
@@ -0,0 +1,7 @@
+module hunt.io;
+
+public import hunt.io.common;
+public import hunt.io.ByteArrayInputStream;
+public import hunt.io.ByteArrayOutputStream;
+public import hunt.io.BufferedNetOutputStream;
+public import hunt.io.PipedStream;
