fcc53a51726559d71c8ce38a2b69a5f18a9820fc
heromyth <bitworld@qq.com>
2018-09-25 09:56:34 +0800

    TreeMapDemo updated
    Former-commit-id: 238ee4df0530667b077f3f892d9327b290c85383

--- examples/ContainerDemo/source/HashMapDemo.d
+++ examples/ContainerDemo/source/HashMapDemo.d
@@ -97,4 +97,94 @@ class HashMapDemo
             writeln(p.toString() ~ " ==> " ~ map.get(p));
         }
     }
+
+    
+    void testHashMapForeach() {
+        // https://stackoverflow.com/questions/4234985/how-to-for-each-the-hashmap
+        // HashMap Declaration
+        writeln("Testing HashMap...");
+        HashMap!(int, string) hmap = new HashMap!(int, string)();
+
+        //Adding elements to LinkedHashMap
+        hmap.put(22, "Abey");
+        hmap.put(33, "Dawn");
+        hmap.put(1, "Sherry");
+        hmap.put(2, "Karon");
+        hmap.put(100, "Jim");
+
+        writeln(hmap.toString());
+
+        writeln("\nTesting HashMap foreach1...");
+        foreach (int key, string v; hmap) {
+            writeln("Key is: " ~ key.to!string ~ " & Value is: " ~ v);
+        }
+
+        writeln("\nTesting HashMap foreach2...");
+        foreach (MapEntry!(int, string) entry; hmap) {
+            writeln("Key is: " ~ entry.getKey().to!string ~ " & Value is: " ~ entry.getValue());
+        }
+
+        writeln("\nTesting HashMap byKey1...");
+        // Iterator!int keyIterator  = hmap.byKey();
+        // while(keyIterator.hasNext)
+        // {
+        //         writeln("Key is: " ~ keyIterator.next().to!string());
+        // }
+
+        InputRange!int keyIterator = hmap.byKey();
+        while (!keyIterator.empty) {
+            writeln("Key is: " ~ keyIterator.front.to!string());
+            keyIterator.popFront();
+        }
+
+        writeln("\nTesting HashMap byKey2...");
+        foreach (int key; hmap.byKey) {
+            writeln("Key is: " ~ key.to!string());
+        }
+
+        writeln("\nTesting HashMap byKey3...");
+        foreach (size_t index, int key; hmap.byKey) {
+            writefln("Key[%d] is: %d ", index, key);
+        }
+
+        writeln("\nTesting HashMap byValue1...");
+        InputRange!string valueIterator = hmap.byValue();
+        while (!valueIterator.empty) {
+            writeln("value is: " ~ valueIterator.front.to!string());
+            valueIterator.popFront();
+        }
+
+        writeln("\nTesting HashMap byValue2...");
+        foreach (string value; hmap.byValue) {
+            writeln("value is: " ~ value);
+        }
+
+        writeln("\nTesting HashMap byValue3...");
+        foreach (size_t index, string value; hmap.byValue) {
+            writefln("value[%d] is: %s ", index, value);
+        }
+
+    }
+
+    void testHashMapRemove() {
+        HashMap!(int, string) hmap = new HashMap!(int, string)();
+
+        //Adding elements to LinkedHashMap
+        hmap.put(22, "Abey");
+        hmap.put(33, "Dawn");
+        hmap.put(1, "Sherry");
+        hmap.put(2, "Karon");
+        hmap.put(100, "Jim");
+
+        writefln("item[%d]=%s", 33, hmap.get(33));
+
+        writeln(hmap.toString());
+
+        assert(hmap.size() == 5);
+        hmap.remove(1);
+
+        writeln(hmap.toString());
+        assert(hmap.size() == 4);
+
+    }
 }
