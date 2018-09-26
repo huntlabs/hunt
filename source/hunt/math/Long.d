2ffc76f0949af31737e58bfb334f9320f4fe10d1
gaoxincheng <gaoxincheng@putao.com>
2018-09-26 11:33:03 +0800

    add override func

--- source/hunt/math/Long.d
+++ source/hunt/math/Long.d
@@ -557,5 +557,38 @@ class Long : Number{
         return cast(int)i & 0x7f;
      }
 
+    override string toString()
+    {
+        return to!string(value);
+    }
 
+    /**
+     * Returns a hash code for a {@code long} value; compatible with
+     * {@code Long.hashCode()}.
+     *
+     * @param value the value to hash
+     * @return a hash code value for a {@code long} value.
+     * @since 1.8
+     */
+    override public size_t toHash() @safe nothrow {
+        return cast(int)(value ^ (value >>> 32));
+    }
+
+    /**
+     * Compares this object to the specified object.  The result is
+     * {@code true} if and only if the argument is not
+     * {@code null} and is a {@code Long} object that
+     * contains the same {@code long} value as this object.
+     *
+     * @param   obj   the object to compare with.
+     * @return  {@code true} if the objects are the same;
+     *          {@code false} otherwise.
+     */
+    override public bool opEquals(Object obj) {
+        auto lg = cast(Long)obj;
+        if (lg !is null) {
+            return value == lg.longValue();
+        }
+        return false;
+    }
 }
