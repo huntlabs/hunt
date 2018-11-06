16743eafb32d3606b5d21f8f7e4b78c86031d935
heromyth <bitworld@qq.com>
2018-11-06 20:33:29 +0800

    UnitTest improved

--- source/hunt/util/UnitTest.d
+++ source/hunt/util/UnitTest.d
@@ -1,51 +1,55 @@
 module hunt.util.UnitTest;
 
-void testUnits(T)()
-{
+void testUnits(T)() {
 	enum v = generateUnitTests!T;
 	// pragma(msg, v);
 	mixin(v);
 }
 
-string generateUnitTests(T)()
-{
+string generateUnitTests(T)() {
 	import std.string;
 	import std.algorithm;
-    import std.traits;
-    
-    enum fullTypeName = fullyQualifiedName!(T);
-    enum memberModuleName = moduleName!(T);
+	import std.traits;
+
+	enum fullTypeName = fullyQualifiedName!(T);
+	enum memberModuleName = moduleName!(T);
 
 	string str;
-    str ~= `import std.stdio;
+	str ~= `import std.stdio;
 writeln("=================================");
 writeln("testing ` ~ fullTypeName ~ `     ");
 writeln("=================================");
 
 `;
-    str ~= "import " ~ memberModuleName ~ ";\n";
-	str ~= "auto t = new "~ T.stringof ~ "();\n";
+	str ~= "import " ~ memberModuleName ~ ";\n";
+	str ~= "auto t = new " ~ T.stringof ~ "();\n";
 
-	foreach (memberName; __traits(derivedMembers, T))
-	{
-		// enum currentMember = __traits(getMember, T, memberName);
-		static if(memberName.startsWith("test") || memberName.endsWith("Test") || 
-			hasUDA!(__traits(getMember, T, memberName), Test))
-		{
-			alias memberType = typeof(__traits(getMember, T, memberName));
-			static if(is(memberType == function))
-			{
-				str ~= `writeln("\n========> running: ` ~ memberName ~ "\");\n";
-				str ~= "t." ~ memberName ~ "();\n";
+	foreach (memberName; __traits(derivedMembers, T)) {
+		// pragma(msg, "member: " ~ memberName);
+		enum memberProtection = __traits(getProtection, __traits(getMember, T, memberName));
+		static if (memberProtection == "private"
+				|| memberProtection == "protected" 
+				|| memberProtection == "export") {
+			// version (HUNT_DEBUG) pragma(msg, "skip private member: " ~ memberName);
+		} else {
+			import std.meta : Alias;
+			alias currentMember = Alias!(__traits(getMember, T, memberName));
+			static if (memberName.startsWith("test") 
+					|| memberName.endsWith("Test")
+					|| hasUDA!(currentMember, Test)) {
+				alias memberType = typeof(currentMember);
+				static if (is(memberType == function)) {
+					str ~= `writeln("\n========> running: ` ~ memberName ~ "\");\n";
+					str ~= "t." ~ memberName ~ "();\n";
+				}
 			}
-		} 
+		}
 	}
 	return str;
 }
 
-
 /**
 */
 struct Test {
-	
+	TypeInfo expected;
 }
