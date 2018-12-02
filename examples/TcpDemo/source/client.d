c41e7c9b6e7ce0d04a6c99189658b5c9234dc01c
heromyth <bitworld@qq.com>
2018-12-02 20:01:59 +0800

    Better exception handling for TcpStream as a client

--- examples/TcpDemo/source/client.d
+++ examples/TcpDemo/source/client.d
@@ -2,51 +2,53 @@ import std.stdio;
 
 import hunt.event;
 import hunt.io.TcpStream;
+
 // import hunt.logging;
 import std.socket;
 
-void main()
-{
+void main() {
 	EventLoop loop = new EventLoop();
 	// TcpStream client = new TcpStream(loop, AddressFamily.INET6);
 	TcpStream client = new TcpStream(loop, AddressFamily.INET);
 	int count = 10;
 	client.onConnected((bool isSucceeded) {
-		if (isSucceeded)
-		{
-			writeln("connected with: ", client.remoteAddress.toString()); 
+		if (isSucceeded) {
+			writeln("connected with: ", client.remoteAddress.toString());
 			client.write(cast(const(ubyte[])) "Hello world!", (in ubyte[] wdata, size_t size) {
 				debug writeln("sent: size=", size, "  content: ", cast(string) wdata);
 			});
-
 			// client.write(new SocketStreamBuffer(cast(const(ubyte[])) "hello world!",
 			// 	(in ubyte[] wdata, size_t size) {
 			// 		debug writeln("sent: size=", size, "  content: ", cast(string) wdata);
 			// 	}));
-		}
-		else
-		{
+		} else {
 			writeln("The connection failed!");
 			loop.stop();
 		}
 	}).onDataReceived((in ubyte[] data) {
 		writeln("received data: ", cast(string) data);
-		if(--count > 0) {
+		if (--count > 0) {
 			client.write(data, (in ubyte[] wdata, size_t size) {
 				debug writeln("sent: size=", size, "  content: ", cast(string) wdata);
 			});
-		// client.write(new SocketStreamBuffer(data.dup, (in ubyte[] wdata, size_t size) {
-		// 		debug writeln("sent: size=", size, "  content: ", cast(string) wdata);
-		// 	}));
+			// client.write(new SocketStreamBuffer(data.dup, (in ubyte[] wdata, size_t size) {
+			// 		debug writeln("sent: size=", size, "  content: ", cast(string) wdata);
+			// 	}));
 		}
 	}).onClosed(() {
-		writeln("The connection is closed!");
+		writeln("The connection closed!");
 		loop.stop();
 	}).connect("127.0.0.1", 8090);
 	// }).connect("::1", 8090);
 	// }).connect("fe80::b6f0:24f9:9b3b:9f28%ens33", 8090);
 	// }).connect("fe80::2435:c2f0:4a2e:ba11%ens33", 8090);
-	
 
 	loop.run();
+
+	// loop.runAsync(20);
+	// writeln("The app will exit in 10 seconds!");
+	// import core.thread;
+	// import core.time;
+	// Thread.sleep(10.seconds);
+	// loop.stop();	
 }
