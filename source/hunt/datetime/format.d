d7de4bb94bfb8b94735a438443787f94770244b8
heromyth <bitworld@qq.com>
2018-12-05 13:33:55 +0800

    Add global GMT time
    Former-commit-id: 825f78583e379b07a34ca04845632e5111ad0e20

--- source/hunt/datetime/format.d
+++ source/hunt/datetime/format.d
@@ -11,7 +11,7 @@
 
 module hunt.datetime.format;
 
-import std.datetime : Month;
+import std.datetime : Month, DayOfWeek;
 
 // return unix timestamp
 int time()
@@ -68,6 +68,36 @@ short monthToShort(Month month)
     }
     
     return resultMonth;
+}
+
+
+string dayAsString(DayOfWeek day) {
+    final switch(day) with(DayOfWeek) {
+        case mon: return "Mon";
+        case tue: return "Tue";
+        case wed: return "Wed";
+        case thu: return "Thu";
+        case fri: return "Fri";
+        case sat: return "Sat";
+        case sun: return "Sun";
+    }
+}
+
+string monthAsString(Month month){
+    final switch(month) with (Month) {
+        case jan: return "Jan";
+        case feb: return "Feb";
+        case mar: return "Mar";
+        case apr: return "Apr";
+        case may: return "May";
+        case jun: return "Jun";
+        case jul: return "Jul";
+        case aug: return "Aug";
+        case sep: return "Sep";
+        case oct: return "Oct";
+        case nov: return "Nov";
+        case dec: return "Dec";
+    }
 }
 
 // return formated time string from timestamp
--- source/hunt/datetime/package.d
+++ source/hunt/datetime/package.d
@@ -1,6 +1,9 @@
 module hunt.datetime;
 
 public import hunt.datetime.format;
+
+import core.atomic;
+import core.thread;
 import std.datetime;
 
 enum TimeUnit : string {
@@ -16,8 +19,71 @@ enum TimeUnit : string {
     Nanosecond = "nsecs"
 }
 
+
+/**
+*/
 class DateTimeHelper {
     static long currentTimeMillis() {
         return convert!(TimeUnit.HectoNanosecond, TimeUnit.Millisecond)(Clock.currStdTime);
+    }
+
+    static string getDateAsGMT() {
+        return cast(string)*httpDate;
+    }
+
+    static void startClock() {
+        if(!atomicLoad(_isClockRunning)) {
+            atomicStore(_isClockRunning, true);
+            dateThread.start(); 
+        }
+    }
+
+    static void stopClock() {
+        atomicStore(_isClockRunning, false);
+    }
+
+    private static shared const(char)[]* httpDate;
+    private static __gshared Thread dateThread;
+    private static shared bool _isClockRunning = false;
+
+    shared static this(){
+        import std.array;
+
+        Appender!(char[])[2] bufs;
+        const(char)[][2] targets;
+        void tick(size_t index) {
+            bufs[index].clear();
+            auto date = Clock.currTime!(ClockType.coarse)(UTC());
+            size_t sz = updateDate(bufs[index], date);
+            targets[index] = bufs[index].data;
+            atomicStore(httpDate, cast(shared)&targets[index]);
+        }
+
+        tick(0);
+        
+        dateThread = new Thread({
+            size_t cur = 1;
+            while(_isClockRunning){ 
+                tick(cur);
+                cur = 1 - cur;
+                Thread.sleep(250.msecs);
+            }
+        });
+    }
+
+    shared static ~this(){
+        stopClock();
+        dateThread.join();
+    }
+
+    private static size_t updateDate(Output, D)(ref Output sink, D date){
+        import std.format;
+        string weekDay = dayAsString(date.dayOfWeek);
+        string month = monthAsString(date.month);
+        return formattedWrite(sink,
+            "%s, %02s %s %04s %02s:%02s:%02s GMT",
+            weekDay, date.day, month, date.year,
+            date.hour, date.minute, date.second
+        );
     }
 }
