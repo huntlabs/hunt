42c38c53347d45d7b830bfa66671d2b129fdf85b
putao <putao@admindeMacBook-Pro.local>
2018-12-06 17:33:12 +0800

    Example improved

--- examples/HttpServer/source/app.d
+++ examples/HttpServer/source/app.d
@@ -14,26 +14,25 @@ import std.stdio;
 import hunt.event;
 import hunt.io.TcpListener;
 import hunt.io.TcpStream;
+import hunt.datetime;
+import hunt.logging;
 import hunt.util.timer;
 
 import std.array;
+import std.conv;
 import std.socket;
 import std.functional;
 import std.getopt;
 import std.exception;
-import hunt.logging;
 import std.datetime;
 
-import datefmt;
-
-void main(string[] args)
-{
+void main(string[] args) {
 	// globalLogLevel(LogLevel.warning);
+	DateTimeHelper.startClock();
 
-	ushort port = 8090;
-	GetoptResult o = getopt(args, "port|p", "Port (default 8090)", &port);
-	if (o.helpWanted)
-	{
+	ushort port = 8080;
+	GetoptResult o = getopt(args, "port|p", "Port (default 8080)", &port);
+	if (o.helpWanted) {
 		defaultGetoptPrinter("A simple http echo server!", o.options);
 		return;
 	}
@@ -48,8 +47,8 @@ void main(string[] args)
 			debug writeln("received: ", cast(string) data);
 
 			//string writeData = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: Keep-Alive\r\nContent-Type: text/plain\r\nServer: Hunt\r\nDate: Wed, 17 Apr 2013 12:00:00 GMT\r\n\r\nHello, World!";
-			
-			string writeData = buildResponse("Hello, World!");
+
+			string writeData = buildResponse("Hello, World! The time is " ~ DateTimeHelper.getDateAsGMT());
 			client.write(cast(ubyte[]) writeData, (in ubyte[] wdata, size_t size) {
 				debug writeln("sent bytes: ", size, "  content: ", cast(string) writeData);
 				// client.close(); // comment out for Keep-Alive
@@ -68,15 +67,14 @@ void main(string[] args)
 	loop.run();
 }
 
-string buildResponse(string content)
-{
+string buildResponse(string content) {
 	Appender!string sb;
 	sb.put("HTTP/1.1 200 OK\r\n");
 	sb.put("Server: Hunt/0.3\r\n");
 	sb.put("Connection: Keep-Alive\r\n");
 	sb.put("Content-Type: text/plain\r\n");
 	sb.put("Content-Length: " ~ to!string(content.length) ~ "\r\n");
-	sb.put("Date: " ~ Clock.currTime.format("%a, %d %b %Y %H:%M:%S GMT"));
+	sb.put("Date: " ~ DateTimeHelper.getDateAsGMT());
 	sb.put("\r\n\r\n");
 	sb.put(content);
 	return sb.data;
