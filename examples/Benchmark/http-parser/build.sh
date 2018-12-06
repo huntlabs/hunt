25ffd27e80cbeb571bb7822eeb671a79f0ce1409
heromyth <bitworld@qq.com>
2018-12-06 18:46:20 +0800

    Update building script for http-parser

--- examples/Benchmark/http-parser/build.sh
+++ examples/Benchmark/http-parser/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/nodejs/http-parser.git .
+git init
+git remote add origin https://github.com/nodejs/http-parser.git
+git fetch
+git branch master origin/master
+git checkout master -f
 make package
