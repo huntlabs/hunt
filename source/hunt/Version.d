c876e15cb78e7f8e43245660e6889acdbf842939
zoujiaqing <zoujiaqing@gmail.com>
2019-01-08 18:28:56 +0800

    Add Version.d to defined hunt versions && update copyright year to 2019
    Former-commit-id: ec07718bad9398778295b9765eff6b6d5f3cfc2a

---
+++ source/hunt/Version.d
@@ -0,0 +1,18 @@
+/*
+ * Hunt - A refined core library for D programming language.
+ *
+ * Copyright (C) 2015-2019 HuntLabs
+ *
+ * Website: https://www.huntlabs.net/
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+ 
+module hunt.Version;
+
+// define hunt versions
+enum HUNT_MAJOR_VERSION = 1;
+enum HUNT_MINOR_VERSION = 0;
+enum HUNT_PATCH_VERSION = 0;
+
