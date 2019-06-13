211408d15f97e9513c01d4a48807e05776a3ec7e
heromyth <bitworld@qq.com>
2019-06-13 09:10:01 +0800

    SimpleQueue's enqueue works improperly

--- examples/UnitTest/source/test/MagedQueueTest.d
+++ examples/UnitTest/source/test/MagedQueueTest.d
@@ -23,8 +23,6 @@ class MagedQueueTest {
 
         q.enqueue(1);
         assert(!q.isEmpty());
-
-        assert(!q.isEmpty());
         q.enqueue(2);
 
         v = q.dequeue();
@@ -35,4 +33,34 @@ class MagedQueueTest {
         assert(v == 2);
         assert(q.isEmpty());
     }
+
+    void testSimpleQueue1() {
+
+        SimpleQueue!long q = new SimpleQueue!long();
+        assert(q.isEmpty());
+        long v;
+
+        q.enqueue(1);
+        assert(q.tryDequeue(v));
+        assert(v == 1);
+        assert(q.isEmpty());
+        assert(!q.tryDequeue(v));
+
+        q.enqueue(1);
+        v = q.dequeue();
+        assert(v == 1);
+        assert(q.isEmpty());
+
+        q.enqueue(1);
+        assert(!q.isEmpty());
+        q.enqueue(2);
+
+        v = q.dequeue();
+        assert(v == 1);
+        assert(!q.isEmpty());
+        
+        v = q.dequeue();
+        assert(v == 2);
+        assert(q.isEmpty());
+    }    
 }
