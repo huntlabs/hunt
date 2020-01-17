97a30c1c1969620e6a80db0bdf16b2fa1b6ff763
heromyth <bitworld@qq.com>
2020-01-17 17:42:16 +0800

    buffer should be cloned

--- source/hunt/io/TcpInputStream.d
+++ source/hunt/io/TcpInputStream.d
@@ -4,48 +4,61 @@ import hunt.io.Common;
 import hunt.io.TcpStream;
 import hunt.logging.ConsoleLogger;
 
+import core.time;
 import std.algorithm;
 import std.format;
 
 import hunt.collection.ByteBuffer;
+import hunt.collection.BufferUtils;
 import hunt.concurrency.SimpleQueue;
 
-
+/**
+ * 
+ */
 class TcpInputStream : InputStream {
 
-    private TcpStream tcp;
-    private SimpleQueue!ByteBuffer bufferQueue;
+    private Duration _timeout;
+    private TcpStream _tcp;
+    private SimpleQueue!ByteBuffer _bufferQueue;
 
-    this(TcpStream tcp) {
+    this(TcpStream tcp, Duration timeout = 5.seconds) {
         assert(tcp !is null);
-        bufferQueue = new SimpleQueue!ByteBuffer();
-        this.tcp = tcp;
-        this.tcp.onReceived(&dataReceived);
+        _bufferQueue = new SimpleQueue!ByteBuffer();
+        _timeout = timeout;
+        _tcp = tcp;
+        _tcp.onReceived(&dataReceived);
     }
 
     private void dataReceived(ByteBuffer buffer) {
-        version(HUNT_IO_DEBUG) trace("data enqueue...");
-        bufferQueue.enqueue(buffer);
+        int size = buffer.remaining();
+        if(size == 0) {
+            warningf("Empty data");
+        } else {
+            // clone the buffer for data safe
+            ByteBuffer copy = BufferUtils.allocate(size);
+            copy.put(buffer).flip();
+            version(HUNT_NET_DEBUG) tracef("data enqueue (%s)...", copy.toString());
+            _bufferQueue.enqueue(copy); 
+        }
     }
 
     override int read(byte[] b, int off, int len) {
-        version(HUNT_IO_DEBUG) info("waitting....");
+        version(HUNT_NET_DEBUG) info("waitting for data....");
         // TODO: Tasks pending completion -@zxp at 7/20/2019, 11:32:52 AM
         // Support timeout
-        ByteBuffer buffer = bufferQueue.dequeue();
+        ByteBuffer buffer = _bufferQueue.dequeue(_timeout);
         int r = buffer.remaining();
-        version(HUNT_IO_DEBUG) info("read....", buffer.toString());
+        version(HUNT_NET_DEBUG) info("read....", buffer.toString());
         r =  min(len, r);
 
-        // buffer.get(b, off, r);
         b[off .. off + r] = buffer.array[0 .. r];
         return r;
     }
 
     override int read() {
-        version(HUNT_IO_DEBUG) trace("waitting....");
-        ByteBuffer buffer = bufferQueue.dequeue();
-        version(HUNT_IO_DEBUG) trace("read....");
+        version(HUNT_NET_DEBUG) trace("waitting....");
+        ByteBuffer buffer = _bufferQueue.dequeue(_timeout);
+        version(HUNT_NET_DEBUG) trace("read....");
         return buffer.get();
     }
 
