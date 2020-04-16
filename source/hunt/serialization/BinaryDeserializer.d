b3090a3bfe5176d4b7fa3362c9230c01d526ecc1
heromyth <bitworld@qq.com>
2020-04-16 10:48:02 +0800

    Minor updates
    Former-commit-id: b36f38ae927ba2a783bf7fa35ff6f3423fe83849

--- source/hunt/serialization/BinaryDeserializer.d
+++ source/hunt/serialization/BinaryDeserializer.d
@@ -3,77 +3,74 @@ module hunt.serialization.BinaryDeserializer;
 import hunt.serialization.specify;
 import std.traits;
 
+/**
+ * 
+ */
 struct BinaryDeserializer {
 
-  private
-  {
-    const (ubyte)[] _buffer;
-  }
+    private {
+        const(ubyte)[] _buffer;
+    }
 
-  this (ubyte[]  buffer)
-  {
-    this._buffer = buffer;
-  }
+    this(ubyte[] buffer) {
+        this._buffer = buffer;
+    }
 
-  const(ubyte[]) bytes() const nothrow  {
-    return _buffer;
-  }
+    const(ubyte[]) bytes() const nothrow {
+        return _buffer;
+    }
 
-  ulong bytesLeft() const { return _buffer.length; }
+    ulong bytesLeft() const {
+        return _buffer.length;
+    }
 
-  T iArchive(T)() if(!isDynamicArray!T && !isAssociativeArray!T && !is(T == class) && __traits(compiles, T()))
-  {
-    T obj;
-    specify (this, obj);
-    return obj;
-  }
+    T iArchive(T)()
+            if (!isDynamicArray!T && !isAssociativeArray!T && !is(T == class) && __traits(compiles, T())) {
+        T obj;
+        specify(this, obj);
+        return obj;
+    }
 
-  T iArchive(T)() if(!isDynamicArray!T && !isAssociativeArray!T &&!is(T == class) && !__traits(compiles, T()))
-  {
-    T obj = void;
-    specify (this, obj);
-    return obj;
-  }
+    T iArchive(T)()
+            if (!isDynamicArray!T && !isAssociativeArray!T && !is(T == class)
+                && !__traits(compiles, T())) {
+        T obj = void;
+        specify(this, obj);
+        return obj;
+    }
 
-  T iArchive(T, A...)(A args) if(is(T == class))
-  {
-    T obj = new T(args);
-    specify(this, obj);
-    return obj;
-  }
+    T iArchive(T, A...)(A args) if (is(T == class)) {
+        T obj = new T(args);
+        specify(this, obj);
+        return obj;
+    }
 
-  T iArchive(T)() if(isDynamicArray!T || isAssociativeArray!T)
-  {
-    return iArchive!(T, ushort)();
-  }
+    T iArchive(T)() if (isDynamicArray!T || isAssociativeArray!T) {
+        return iArchive!(T, ushort)();
+    }
 
-  T iArchive(T , U)() if(isDynamicArray!T || isAssociativeArray!T)
-  {
-    T obj;
-    specify!U(this, obj);
-    return obj;
-  }
+    T iArchive(T, U)() if (isDynamicArray!T || isAssociativeArray!T) {
+        T obj;
+        specify!U(this, obj);
+        return obj;
+    }
 
-  void putUbyte(ref ubyte val)
-  {
-    val = _buffer[0];
-    _buffer = _buffer[1..$];
-  }
+    void putUbyte(ref ubyte val) {
+        val = _buffer[0];
+        _buffer = _buffer[1 .. $];
+    }
 
+    void putClass(T)(T val) if (is(T == class)) {
+        specifyClass(this, val);
+    }
 
-  void putClass(T)(T val) if(is(T == class))
-  {
-    specifyClass(this, val);
-  }
+    auto putRaw(ushort length) {
+        auto res = _buffer[0 .. length];
+        _buffer = _buffer[length .. $];
+        return res;
+    }
 
-  auto putRaw(ushort length) {
-    auto res = _buffer[0 .. length];
-    _buffer = _buffer[length .. $];
-    return res;
-  }
-
-  bool isNullObj()
-  {
-    return _buffer[0 .. 4] == NULL ? true : false;
-  }
+    bool isNullObj() {
+        return _buffer[0 .. 4] == NULL ? true : false;
+    }
 }
