04396cede0c917f06b257b972fdae57a402bc8be
linsen <linsen_519@163.com>
2020-04-20 22:42:43 -0400

    format file

--- examples/TcpDemo/source/server.d
+++ examples/TcpDemo/source/server.d
@@ -28,32 +28,32 @@ import std.process;
 
 void main()
 {
-	debug writefln("Main thread: %s", getTid());
-	// globalLogLevel(LogLevel.warning);
 
+    debug writefln("Main thread: %s", getTid());
+    // globalLogLevel(LogLevel.warning);
 
-	EventLoop loop = new EventLoop();
-	TcpListener listener = new TcpListener(loop, AddressFamily.INET, 512);
+    EventLoop loop = new EventLoop();
+    TcpListener listener = new TcpListener(loop, AddressFamily.INET, 512);
 
-  listener.bind(8080)
-  .listen(1024)
-  .error((IoError error){writefln("error occurred: %d  %s",error.errorCode , error.errorMsg);})
-	.accepted((TcpListener sender, TcpStream client) {
-		debug writefln("new connection from: %s", client.remoteAddress.toString());
-		client.received((ByteBuffer buffer) {
-			ubyte[] data = cast(ubyte[])buffer.getRemaining();
-			debug writeln("received bytes: ", data.length);
-			const(ubyte)[] sentData = data; // echo test
-			client.write(sentData);
-		}).disconnected(() {
-			debug writefln("client disconnected: %s", client.remoteAddress.toString());
-		}).closed(() {
-			debug writefln("connection closed, local: %s, remote: %s",
-			client.localAddress.toString(), client.remoteAddress.toString());
-		}).error((IoError error){
-			  writefln("error occurred: %d  %s",error.errorCode , error.errorMsg);
-		});
-	})
+    listener.bind(8080)
+    .listen(1024)
+    .error((IoError error){writefln("error occurred: %d  %s",error.errorCode , error.errorMsg);})
+    .accepted((TcpListener sender, TcpStream client) {
+        debug writefln("new connection from: %s", client.remoteAddress.toString());
+        client.received((ByteBuffer buffer) {
+          ubyte[] data = cast(ubyte[])buffer.getRemaining();
+          debug writeln("received bytes: ", data.length);
+          const(ubyte)[] sentData = data; // echo test
+          client.write(sentData);
+        }).disconnected(() {
+          debug writefln("client disconnected: %s", client.remoteAddress.toString());
+        }).closed(() {
+          debug writefln("connection closed, local: %s, remote: %s",
+          client.localAddress.toString(), client.remoteAddress.toString());
+        }).error((IoError error){
+            writefln("error occurred: %d  %s",error.errorCode , error.errorMsg);
+        });
+    })
 	.start();
 
 	writeln("Listening on: ", listener.bindingAddress.toString());
